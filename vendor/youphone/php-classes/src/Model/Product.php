53020bc4595a391cf02dfbc7b554486706516347
Eduardo Henrique <eduardo.horoutlook.com>
2020-06-08 23:45:56 -0300

    Detalhes do Produto

--- vendor/youphone/php-classes/src/Model/Product.php
+++ vendor/youphone/php-classes/src/Model/Product.php
@@ -149,5 +149,33 @@ class Product extends Model {
 
 	}
 
+	public function getFromUrl($desurl)
+	{
+
+		$sql = new Sql();
+
+		$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl", [
+			':desurl'=>$desurl
+		]);
+
+		$this->setData($rows[0]);
+
+	}
+
+	
+	public function getCategories()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("
+			SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
+		", [
+
+			':idproduct'=>$this->getidproduct()
+		]);
+
+	}
+
 }
  ?>
