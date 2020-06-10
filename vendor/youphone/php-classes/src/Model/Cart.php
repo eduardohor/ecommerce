c3ff4bc99699145a5529651a5542db18495e9e28
Eduardo Henrique <eduardo.horoutlook.com>
2020-06-09 22:56:23 -0300

    WEB Service Endereço

--- vendor/youphone/php-classes/src/Model/Cart.php
+++ vendor/youphone/php-classes/src/Model/Cart.php
@@ -10,7 +10,7 @@ use \Youphone\Model\User;
 class Cart extends Model {
 
 	const SESSION = "Cart";
-	const ERROR = "CartError";
+	const SESSION_ERROR = "CartError";
 
 	public static function getFromSession()
 	{
@@ -123,6 +123,8 @@ class Cart extends Model {
 			':idproduct'=>$product->getidproduct()
 		]);
 
+		$this->getCalculateTotal();
+
 	}
 
 	public function removeProduct(Product $product, $all = false)
@@ -145,6 +147,8 @@ class Cart extends Model {
 			]);
 
 		}
+
+		$this->getCalculateTotal();
 
 	}
 
@@ -174,41 +178,122 @@ class Cart extends Model {
 		$sql = new Sql();
 
 		$results = $sql->select("
-			SELECT COUNT(*) AS nrqtd, SUM(b.vlprice) AS vlprice, SUM(b.vlwidth) AS vlwidth, SUM(b.vlheight) AS vlheight, SUM(b.vllength) AS vllength, SUM(b.vlweight) AS vlweight
-			FROM tb_cartsproducts a
-			INNER JOIN tb_products b ON a.idproduct = b.idproduct
-			WHERE a.idcart = :idcart AND a.dtremoved IS NULL
+			SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
+			FROM tb_products a
+			INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
+			WHERE b.idcart = :idcart AND dtremoved IS NULL;
 		", [
 			':idcart'=>$this->getidcart()
 		]);
 
 		if (count($results) > 0) {
-
 			return $results[0];
+		} else {
+			return [];
+		}
+
+	}
+
+	public function setFreight($nrzipcode)
+	{
+
+		$nrzipcode = str_replace('-', '', $nrzipcode);
+
+		$totals = $this->getProductsTotals();
+
+		if ($totals['nrqtd'] > 0) {
+
+			if ($totals['vlheight'] < 2) $totals['vlheight'] = 2;
+			if ($totals['vllength'] < 16) $totals['vllength'] = 16;
+
+			$qs = http_build_query([
+				'nCdEmpresa'=>'',
+				'sDsSenha'=>'',
+				'nCdServico'=>'40010',
+				'sCepOrigem'=>'09853120',
+				'sCepDestino'=>$nrzipcode,
+				'nVlPeso'=>$totals['vlweight'],
+				'nCdFormato'=>'1',
+				'nVlComprimento'=>$totals['vllength'],
+				'nVlAltura'=>$totals['vlheight'],
+				'nVlLargura'=>$totals['vlwidth'],
+				'nVlDiametro'=>'0',
+				'sCdMaoPropria'=>'S',
+				'nVlValorDeclarado'=>$totals['vlprice'],
+				'sCdAvisoRecebimento'=>'S'
+			]);
+
+			$xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
+
+			$result = $xml->Servicos->cServico;
+
+			if ($result->MsgErro != '') {
+
+				Cart::setMsgError($result->MsgErro);
+
+			} else {
+
+				Cart::clearMsgError();
+
+			}
+
+			$this->setnrdays($result->PrazoEntrega);
+			$this->setvlfreight(Cart::formatValueToDecimal($result->Valor));
+			$this->setdeszipcode($nrzipcode);
+
+			$this->save();
+
+			return $result;
 
 		} else {
 
-			return [
-				'vlprice'=>0,
-				'vlweight'=>0,
-				'vlheight'=>0,
-				'vllength'=>0,
-				'vlweight'=>0
-			];
+
 
 		}
 
 	}
 
-	public function getCalculateTotal()
+	public static function formatValueToDecimal($value):float
 	{
 
-		$totals = $this->getProductsTotals();
+		$value = str_replace('.', '', $value);
+		return str_replace(',', '.', $value);
 
-		$this->updateFreight();
+	}
 
-		$this->setvlsubtotal($totals['vlprice']);
-		$this->setvltotal($totals['vlprice'] + $this->getvlfreight());
+	public static function setMsgError($msg)
+	{
+
+		$_SESSION[Cart::SESSION_ERROR] = $msg;
+
+	}
+
+	public static function getMsgError()
+	{
+
+		$msg = (isset($_SESSION[Cart::SESSION_ERROR])) ? $_SESSION[Cart::SESSION_ERROR] : "";
+
+		Cart::clearMsgError();
+
+		return $msg;
+
+	}
+
+	public static function clearMsgError()
+	{
+
+		$_SESSION[Cart::SESSION_ERROR] = NULL;
+
+	}
+
+	public function updateFreight()
+	{
+
+		if ($this->getdeszipcode() != '') {
+
+			$this->setFreight($this->getdeszipcode());
+
+		}
 
 	}
 
@@ -217,114 +302,22 @@ class Cart extends Model {
 
 		$this->getCalculateTotal();
 
-		$values = parent::getValues();
-
-		return $values;
+		return parent::getValues();
 
 	}
 
-	public function setFreight($zipcode)
+	public function getCalculateTotal()
 	{
+
+		$this->updateFreight();
 
 		$totals = $this->getProductsTotals();
 
-		if ($totals['nrqtd'] === 0) {
-
-			$this->setvlfreight(NULL);
-			$this->setnrdays(NULL);
-			$this->setdeszipcode(NULL);
-
-			$this->save();
-
-			return false;
-
-		}
-
-		$vllength = ($totals['vllength'] < 16) ? 16 : $totals['vllength'];
-
-		$qs = http_build_query([
-			"nCdEmpresa"=>"",
-			"sDsSenha"=>"",
-			"nCdServico"=>"40010",
-			"sCepOrigem"=>"65400000",
-			"sCepDestino"=>$zipcode,
-			"nVlPeso"=>$totals['vlweight'],
-			"nCdFormato"=>"1",
-			"nVlComprimento"=>$vllength,
-			"nVlAltura"=>$totals['vlheight'],
-			"nVlLargura"=>$totals['vlwidth'],
-			"nVlDiametro"=>"0",
-			"sCdMaoPropria"=>"S",
-			"nVlValorDeclarado"=>$totals['vlprice'],
-			"sCdAvisoRecebimento"=>"S"
-		]);
-
-		$url = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs;
-
-		$xml = simplexml_load_file($url);
-		$response = (array)$xml->Servicos->cServico;
-
-		if ($response['MsgErro'] != '') {
-
-			$this->setCartError($response['MsgErro']);
-
-		}
-
-		$this->setvlfreight(Cart::valueToDecimal($response['Valor']));
-		$this->setnrdays($response['PrazoEntrega']);
-		$this->setdeszipcode($zipcode);
-
-		$this->save();
-
-		return true;
-
-	}
-
-	public static function valueToDecimal($value)
-	{
-
-		$value = str_replace('.', '', $value);
-		return str_replace(',', '.', $value);
-
-	}
-
-	public function setCartError($msg)
-	{
-
-		$_SESSION[Cart::ERROR] = $msg;
-
-	}
-
-	public function getCartError()
-	{
-
-		$msg = (isset($_SESSION[Cart::ERROR]) && $_SESSION[Cart::ERROR]) ? $_SESSION[Cart::ERROR] : '';
-
-		$this->clearCartError();
-
-		return $msg;
-
-	}
-
-	public function clearCartError()
-	{
-
-		$_SESSION[Cart::ERROR] = NULL;
-
-	}
-
-	public function updateFreight()
-	{
-
-		if ($this->getdeszipcode()) {
-
-			$this->setFreight($this->getdeszipcode());
-
-		}
+		$this->setvlsubtotal($totals['vlprice']);
+		$this->setvltotal($totals['vlprice'] + (float)$this->getvlfreight());
 
 	}
 
 }
-
 
  ?>
--- views-cache/login.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/login.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,5 +1,4 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>
-
+<?php if(!class_exists('Rain\Tpl')){exit;}?> 
 <div class="product-big-title-area">
     <div class="container">
         <div class="row">
@@ -17,16 +16,18 @@
     <div class="container">
         <div class="row">                
             <div class="col-md-6">
+
+                <?php if( $error != '' ){ ?>
+
+                <div class="alert alert-danger">
+                    <?php echo htmlspecialchars( $error, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+
+                </div>
+                <?php } ?>
+
+
                 <form action="/login" id="login-form-wrap" class="login" method="post">
                     <h2>Acessar</h2>
-                    <?php if( $loginError!='' ){ ?>
-
-                    <div class="alert alert-danger">
-                        <?php echo htmlspecialchars( $loginError, ENT_COMPAT, 'UTF-8', FALSE ); ?>
-
-                    </div>
-                    <?php } ?>
-
                     <p class="form-row form-row-first">
                         <label for="login">E-mail <span class="required">*</span>
                         </label>
@@ -50,10 +51,11 @@
                 </form>                    
             </div>
             <div class="col-md-6">
-                <?php if( $registerError!='' ){ ?>
+                
+                <?php if( $errorRegister != '' ){ ?>
 
                 <div class="alert alert-danger">
-                    <?php echo htmlspecialchars( $registerError, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+                    <?php echo htmlspecialchars( $errorRegister, ENT_COMPAT, 'UTF-8', FALSE ); ?>
 
                 </div>
                 <?php } ?>
@@ -64,17 +66,17 @@
                     <p class="form-row form-row-first">
                         <label for="nome">Nome Completo <span class="required">*</span>
                         </label>
-                        <input type="text" id="nome" name="name" class="input-text" value="<?php echo htmlspecialchars( $postValues["name"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                        <input type="text" id="nome" name="name" class="input-text" value="<?php echo htmlspecialchars( $registerValues["name"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
                     </p>
                     <p class="form-row form-row-first">
                         <label for="email">E-mail <span class="required">*</span>
                         </label>
-                        <input type="email" id="email" name="email" class="input-text" value="<?php echo htmlspecialchars( $postValues["email"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                        <input type="email" id="email" name="email" class="input-text" value="<?php echo htmlspecialchars( $registerValues["email"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
                     </p>
                     <p class="form-row form-row-first">
                         <label for="phone">Telefone
                         </label>
-                        <input type="text" id="phone" name="phone" class="input-text" value="<?php echo htmlspecialchars( $postValues["phone"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                        <input type="text" id="phone" name="phone" class="input-text" value="<?php echo htmlspecialchars( $registerValues["phone"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
                     </p>
                     <p class="form-row form-row-last">
                         <label for="senha">Senha <span class="required">*</span>
