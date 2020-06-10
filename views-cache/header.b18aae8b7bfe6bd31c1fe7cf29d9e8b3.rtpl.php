5eba343d5df57acb173685154ad97f5b729eb094
Eduardo Henrique <eduardo.horoutlook.com>
2020-06-10 18:04:17 -0300

    Admin-Pedidos

--- views-cache/header.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/header.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -258,19 +258,10 @@ desired effect
         <!-- Optionally, you can add icons to the links -->
         <li><a href="/admin/users"><i class="fa fa-users"></i> <span>Usuários</span></a></li>
         <li><a href="/admin/categories"><i class="fa fa-link"></i> <span>Categorias</span></a></li>
-         <li><a href="/admin/products"><i class="fa fa-link"></i> <span>Produtos</span></a></li>
+        <li><a href="/admin/products"><i class="fa fa-link"></i> <span>Produtos</span></a></li>
+        <li><a href="/admin/orders"><i class="fa fa-shopping"></i> <span>Pedidos</span></a></li>
 
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#">Link in level 2</a></li>
-            <li><a href="#">Link in level 2</a></li>
-          </ul>
-        </li>
+        
       </ul>
       <!-- /.sidebar-menu -->
     </section>
